79d8068e364690194d4aa51328c3dc485141a022
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-03-25 22:27:58 +0530

    Add files via upload

---
+++ JAVA/Primitives.java
@@ -0,0 +1,29 @@
+package ravi;
+
+import java.util.Scanner;
+
+public class Primitives {
+    public static void main(String[] args) {
+        Scanner examples = new Scanner(System.in);
+//        int rollno = 64;
+//        char name = 'r';
+//        float deci = 22.11f;
+//        double long_deci = 2832233.3132;
+//        long long_int = 1232331242434L;
+//        boolean check = true;
+
+//        String word = examples.nextLine();
+//        System.out.println(word);
+
+        int num = examples.nextInt();
+        System.out.println("number "+ num);
+
+        float deci_num = examples.nextFloat();
+        System.out.println("Float "+ deci_num);
+
+
+
+
+
+    }
+}
