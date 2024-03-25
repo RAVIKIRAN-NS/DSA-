73607ace5e35319478d70bdaa969289389eaa111
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-03-25 22:26:48 +0530

    Add files via upload

---
+++ JAVA/Inputs.java
@@ -0,0 +1,17 @@
+package ravi;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+
+//        Scanner ravi = new Scanner(System.in);
+//        System.out.println(ravi.nextLine());
+
+          Scanner students = new Scanner(System.in);
+          System.out.print("Enter the Roll no : ");
+          int roll_no = students.nextInt();
+          System.out.println("Your Roll no is " + roll_no);
+
+    }
+}
