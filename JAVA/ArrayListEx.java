5521b6433b0a4f56344a608765d2d5f2cef0f3a1
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-04-08 22:58:21 +0530

    Add files via upload

---
+++ JAVA/ArrayListEx.java
@@ -0,0 +1,29 @@
+package ravi;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        Scanner al = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        list.add(12);
+        list.add(33);
+        list.add(99);
+        list.add(71);
+
+        System.out.println(list);
+        list.set(0,100);
+        System.out.println(list);
+
+        for (int i = 0;i < 6;i++){
+            list.add(i);
+            System.out.println(list.get(i));
+        }
+        System.out.println(list);
+
+
+
+    }
+}
