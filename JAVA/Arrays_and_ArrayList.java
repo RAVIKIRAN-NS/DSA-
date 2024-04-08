076a9589b404e014615aaba0b806d3a5d3bffbfd
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-04-08 22:58:40 +0530

    Add files via upload

---
+++ JAVA/Arrays_and_ArrayList.java
@@ -0,0 +1,12 @@
+package ravi;
+
+public class Arrays_and_ArrayList {
+    public static void main(String[] args) {
+        int[] num ;
+        num = new int[5];
+        System.out.println(num[2]);
+
+        String[] names = new String[5];
+        System.out.println(names[0]);
+    }
+}
