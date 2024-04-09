2bd9b6d90cc3d5d1aa31ee633ec0ae12d71e51cb
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-04-09 23:55:20 +0530

    Add files via upload

---
+++ JAVA/Loops.java
@@ -0,0 +1,27 @@
+package ravi;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+//        Scanner num = new Scanner(System.in);
+//        int n = num.nextInt();
+//
+//        for (int i = 1;i < n;i++){
+//            System.out.println(i);
+//            System.out.println("hello");
+//        }
+
+//        int j = 1;
+//
+//        while (j <= 5){
+//            System.out.println(j);
+//            j++;
+//        }
+        int k = 1;
+
+        do {
+            System.out.println("hello");
+        } while (k != 1);
+    }
+}
