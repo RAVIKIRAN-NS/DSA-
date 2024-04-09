6f6af5c1c6149c9ac3f175929e13cd5016d94d4a
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-04-10 00:01:51 +0530

    Add files via upload

---
+++ JAVA/passingFunctions.java
@@ -0,0 +1,16 @@
+package ravi;
+
+import java.util.Arrays;
+
+public class passingFunctions {
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,3};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+    static void change(int[] arr){
+        arr[0] = 99;
+    }
+}
