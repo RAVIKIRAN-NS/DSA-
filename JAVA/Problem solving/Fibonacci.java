fcf7ca3fb3ad433900245dbc9b9ddbd57caaa4a9
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-04-12 07:05:26 +0530

    Add files via upload

---
+++ JAVA/Problem solving/Fibonacci.java
@@ -0,0 +1,21 @@
+package ravi;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int n = num.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n){
+            int temp = b;
+            b = b+a;
+            a = temp;
+            count++;
+        }
+        System.out.println(b);
+    }
+}
