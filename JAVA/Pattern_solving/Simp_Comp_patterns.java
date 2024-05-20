6b7e1f84e86cdbb12b0dd403e2ebd081ab5c08bb
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-05-20 22:35:59 +0530

    Update Simp_Comp_patterns.java

--- JAVA/Pattern_solving/Simp_Comp_patterns.java
+++ JAVA/Pattern_solving/Simp_Comp_patterns.java
@@ -40,4 +40,18 @@ public class pattern_1 {
             System.out.println();
         }
     }
+     static void pattern30(int n){
+        for (int row = 0;row <= n;row++){
+            for (int space = 0;space < n-row;space++){
+                System.out.print("  ");
+            }
+            for (int col = row;col >= 1;col--){
+                System.out.print(col+" ");
+            }
+            for (int col = 2;col <= row;col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
 }
