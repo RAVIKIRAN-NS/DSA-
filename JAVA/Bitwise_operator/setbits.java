e8fde11ba81c6112f4c1f4383912bc09cfd8f8af
RAVIKIRAN NS <140703924+RAVIKIRAN-NS@users.noreply.github.com>
2024-07-10 23:34:28 +0530

    Add files via upload

---
+++ JAVA/Bitwise_operator/setbits.java
@@ -0,0 +1,8 @@
+package ravi.bitwise_operator;
+
+public class setbits {
+    public static void main(String[] args) {
+        int n = 128888;
+        System.out.println(Integer.toBinaryString(n));
+    }
+}
